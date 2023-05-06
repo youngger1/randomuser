b6a41191031c43a1efb176ac4534e2db5a950f6f
youngger1 <youngger1@helixtech.co.kr>
2023-05-06 15:31:28 +0900

    Initial Commit

---
+++ randomuser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randomuser
+//
+//  Created by 김상우 on 2023/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
